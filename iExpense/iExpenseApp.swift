874037110a151c46b00e37a41c1004a88bff399d
dhruvbhardwaj14 <dhruv9891891827@gmail.com>
2022-06-30 21:29:21 +0530

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Dhruv Bhardwaj on 30/06/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
